460c5ae84768b9c3961633ee5679973686f2b9d4
testaccount4568 <92040398+testaccount4568@users.noreply.github.com>
2021-11-17 23:47:40 -0500

    Update application.py

--- application.py
+++ application.py
@@ -1,10 +1,10 @@
 from website import create_app
 
-app = create_app()
+application = create_app()
 # This calls __init__.py and runs create app which creates a new Flask class object that gets configured in init.py and then returns it to
 # this class so that if this class so that the next few lines can be run. These next lines will make sure that if the file is imported and
 # therfore not the main class, then the app will not run
 
 if __name__ == '__main__': # if the __name__ of this file equals the "__main__" file, then run the imported create_app() file
     #, but if this is being imported then don't run the app
-    app.run(debug=True) # Debug = true means that anytime there is a saved change made to any of these files then the server will automatically rerun
+    application.run(debug=True) # Debug = true means that anytime there is a saved change made to any of these files then the server will automatically rerun
